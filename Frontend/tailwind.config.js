e7feb6713e5b9568d61c56c9e03a2d66a9294761
AO510 <gotouao126@gmail.com>
2024-12-04 22:26:28 +0900

    Delete Frontend directory

--- Frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Tailwind が適用されるファイルパス
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
